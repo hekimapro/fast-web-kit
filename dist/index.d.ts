18b07d77d971574be87b62ed4c38bfb96dba6678
hekima-dev <taprotec@gmail.com>
2023-05-22 09:00:31 +0300

    new changes

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * as time from "./package/time";
-export * as array from "./package/array";
-export * as email from "./package/email";
-export * as string from "./package/string";
-export * as number from "./package/number";
-export * as object from "./package/object";
-export * as password from "./package/password";
